10b26ad60b0d6e807e63001a969bb594333d77c2
Peter Harrington <48932392+pzharrington@users.noreply.github.com>
2022-01-07 12:51:24 -0800

    Drop reservation flag

--- submit_pm.sh
+++ submit_pm.sh
@@ -6,7 +6,6 @@
 #SBATCH --gpus-per-task 1
 #SBATCH --time=0:10:00
 #SBATCH --image=romerojosh/containers:sc21_tutorial
-#SBATCH --reservation=sc21_tutorial_01
 #SBATCH -J pm-crop64
 #SBATCH -o %x-%j.out
 
